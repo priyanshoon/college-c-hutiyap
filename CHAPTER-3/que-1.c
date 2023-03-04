b9a2e8942f7ac946ed3383af64cd595bd579b508
Priyanshu Gupta <guptapriyanshu973@gmail.com>
2023-03-04 19:11:34 +0530

    yoyoy

--- CHAPTER-3/que-1.c
+++ CHAPTER-3/que-1.c
@@ -1,13 +1,23 @@
 #include <stdio.h>
 
 int main() {
-    int i;
+    int i, count_odd = 0, count_even = 0;
     int num[10];
-    printf("Enter 5 integers here: ");
-    for (i = 0; i < 9; ++i) {
+    printf("Enter 10 integers here: ");
+    for (i = 0; i < 10; i++) {
         scanf("%d", &num[i]);
     }
 
+    for (i = 0; i < 10; i++) {
+        if (num[i] % 2 == 0) {
+            count_even++;
+        } else {
+            count_odd++;
+        }
+    }
+
+    printf("Even: %d\n", count_even);
+    printf("Odd: %d\n", count_odd);
 
     return 0;
 }
