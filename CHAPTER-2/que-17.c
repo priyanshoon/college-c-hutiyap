bc58fbf69c1dcbe1ce4506774ef7951f9844e5c4
Priyanshu Gupta <guptapriyanshu973@gmail.com>
2023-03-04 14:15:33 +0530

    yoyo

--- CHAPTER-2/que-17.c
+++ CHAPTER-2/que-17.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
 
 int main() {
+    float total_invest, rate, time, maturity;
+    printf("Enter your total investment: ");
+    scanf("%f", &total_invest);
+    printf("Enter your time period: ");
+    scanf("%f", &time);
 
+    time = time / 365;
+
+    if (time <= 180) {
+        rate = 5.5;
+        maturity = total_invest + ((total_invest * time * rate) / 100);
+        printf("total maturity amount will be : %.2f", maturity);
+    } else if (time >= 181 && time < 364) {
+        rate = 7.5;
+        maturity = total_invest + ((total_invest * time * rate) / 100);
+        printf("total maturity amount will be : %.2f", maturity);
+    } else if (time == 365) {
+        rate = 9.0;
+        maturity = total_invest + ((total_invest * time * rate) / 100);
+        printf("total maturity amount will be : %.2f", maturity);
+    } else {
+        rate = 8.5;
+        maturity = total_invest + ((total_invest * time * rate) / 100);
+        printf("total maturity amount will be : %.2f", maturity);
+    }
     return 0;
 }
