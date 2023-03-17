a06bfa5f1cbf91aac5c6647607d771408b4fd7ea
sudopd <guptapriyanshu973@gmail.com>
2023-03-17 08:54:21 +0530

    added

--- CHAPTER-3/que-3.c
+++ CHAPTER-3/que-3.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 
 int main() {
-    int num;
+    int n, isPrime = 1;
+    printf("Enter the number n: ");
+    scanf("%d", &n);
+
+    for (int i = 2; i*i < n; i++) {
+        if (n%i == 0) {
+            isPrime = 0;
+        }
+    }
+
+    if (isPrime) {
+        printf("The number %d is prime!\n", n);
+    } else {
+        printf("The number %d is no prime!\n", n);
+    }
+
     return 0;
 }
