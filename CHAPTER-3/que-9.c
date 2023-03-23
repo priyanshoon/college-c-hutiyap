5c96c561957d9e83571284979256f767fa60bd06
sudopd <guptapriyanshu973@gmail.com>
2023-03-23 18:24:25 +0530

    hhmmmm

--- CHAPTER-3/que-9.c
+++ CHAPTER-3/que-9.c
@@ -1,6 +1,28 @@
 #include <stdio.h>
 
+void factors(int n) {
+	for (int i = 1; i<n; i++) {
+		if (n%i == 0) {
+			printf("%d ", i);
+		}
+	}
+}
+
+void primeFactors(int n) {
+    for (int i = 2; i*i <= n; i++) {
+        if (n%i == 0) {
+            printf("%d ", i);
+        }
+    }
+}
+
 int main() {
-	
+	int n;
+	printf("Enter your number: ");
+	scanf("%d", &n);
+	factors(n);
+	printf("\n");
+	primeFactors(n);
+	printf("\n");
 	return 0;
 }
