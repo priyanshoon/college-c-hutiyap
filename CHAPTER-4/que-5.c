30725ead356bdb2b61453dd09f02cef088663fb4
Naam me kya rakha hai <guptapriyanshu973@gmail.com>
2023-04-17 17:02:07 +0530

    yoy

---
+++ CHAPTER-4/que-5.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int n, x;
+    printf("Enter the value of x: ");
+    scanf("%d", &x);
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+
+
+
+    return 0;
+}
