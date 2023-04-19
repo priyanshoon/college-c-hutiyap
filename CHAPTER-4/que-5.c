90939fe452b1b7f50e4a16acb3f4d27c196e1974
Naam me kya rakha hai <guptapriyanshu973@gmail.com>
2023-04-20 04:15:13 +0530

    chap-4-5

--- CHAPTER-4/que-5.c
+++ CHAPTER-4/que-5.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
+#include <math.h>
 
 int main() {
+    int x, n, odd_count=1, sum=0, sign=1;
+    printf("Input the vaule of x: ");
+    scanf("%d", &x);
+    printf("Input the no of terms: ");
+    scanf("%d", &n);
 
-	
+    for (int i = 1; i <= n; i++) {
+        sum += pow(x, odd_count) * sign;
+        odd_count = odd_count + 2;
+        sign = sign * (-1);
+    }
+
+    printf("the sum = %d\n", sum);
 
 	return 0;
 
---
+++ CHAPTER-4/que-6.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    int start, end;
+    return 0;
+}
