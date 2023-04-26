8564bbdda4ec609b3f1bf20d63a420c909cb864e
Naam me kya rakha hai <guptapriyanshu973@gmail.com>
2023-04-26 06:52:20 +0530

    hmmmm

--- CHAPTER-5/que-9.c
+++ CHAPTER-5/que-9.c
@@ -5,16 +5,13 @@ int main(){
     printf("Enter the number of rows: ");
     scanf("%d",&n);
     int c=n;
-    for(int i=n;i>0;i--)
-    {
-        for(int j=1;j<=i;j++)
-        {
+    for(int i=n;i>0;i--) {
+        for(int j=1;j<=i;j++) {
             printf("%d ",c);
             
         }
         c--;
         printf("\n");
     }
-    
-return 0;
+    return 0;
 }
