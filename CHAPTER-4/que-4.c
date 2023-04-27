44e0ab2a581ce40914086ce184327c8767338183
Naam me kya rakha hai <guptapriyanshu973@gmail.com>
2023-04-27 06:19:07 +0530

    hhmhmh

--- CHAPTER-4/que-4.c
+++ CHAPTER-4/que-4.c
@@ -1,4 +1,5 @@
 #include <math.h>
+#include <stdlib.h>
 #include <stdio.h>
 
 float factorial(int n) {
@@ -22,11 +23,9 @@ int main() {
     printf("value of x = %f\n", x);
 
     for (int i = 1; i < n; i++) {
-        // printf("fac %f\n", even_count/factorial(even_count));
         sign = sign * (-1);
         psum = sign * pow(x, even_count)/factorial(even_count);
         sum = sum + psum;
-        //printf("%f\n", even_count);
         even_count += 2;
     }
 
